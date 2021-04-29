ffeabf2729a0d3ac6a0362e8c5af80d1df8e0abb
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-04-29 03:11:06 -0500

    Update routes styles

--- src/app/routes/login/login.route.tsx
+++ src/app/routes/login/login.route.tsx
@@ -4,44 +4,61 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import { Link as RouterLink } from "react-router-dom";
+
+import "./login.route.scss";
 
 const LoginRoute: React.FunctionComponent = () => {
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className="paper">
-        <Typography component="h1">Login</Typography>
-
-        <form>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            label="Email address"
-            id="email"
-            name="email"
-            autoComplete="email"
-            fullWidth
-            autoFocus
-            required
-          ></TextField>
-
-          <TextField
-            variant="outlined"
-            margin="normal"
-            label="Password"
-            id="password"
-            name="password"
-            autoCapitalize="current-password"
-            fullWidth
-            required
-          ></TextField>
-
-          <Button type="submit" variant="contained" color="primary" fullWidth>
+    <div className="login-route">
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className="paper">
+          <Typography component="h1" variant="h5">
             Sign In
-          </Button>
-        </form>
-      </div>
-    </Container>
+          </Typography>
+
+          <form>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              label="Email address"
+              id="email"
+              name="email"
+              autoComplete="email"
+              fullWidth
+              autoFocus
+              required
+            ></TextField>
+
+            <TextField
+              variant="outlined"
+              margin="normal"
+              label="Password"
+              id="password"
+              name="password"
+              autoCapitalize="current-password"
+              fullWidth
+              required
+            ></TextField>
+
+            <Button
+              className="submit-button"
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Sign In
+            </Button>
+          </form>
+          <Link component={RouterLink} to="/register" variant="body2">
+            {"Don't have an account? Sign Up"}
+          </Link>
+        </div>
+      </Container>
+    </div>
   );
 };
 
--- src/app/routes/register/register.route.tsx
+++ src/app/routes/register/register.route.tsx
@@ -1,42 +1,64 @@
 import React from "react";
-import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import { Link as RouterLink } from "react-router-dom";
+
+import "./register.route.scss";
 
 const RegisterRoute: React.FunctionComponent = () => {
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div>
-        <Typography component="h1">Sign Up</Typography>
+    <div className="register-route">
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
 
-        <form>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            label="Email address"
-            id="email"
-            name="email"
-            autoComplete="email"
-            fullWidth
-            autoFocus
-            required
-          ></TextField>
+          <form>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              label="Email address"
+              id="email"
+              name="email"
+              autoComplete="email"
+              fullWidth
+              autoFocus
+              required
+            ></TextField>
 
-          <TextField
-            variant="outlined"
-            margin="normal"
-            label="Password"
-            id="password"
-            name="password"
-            autoCapitalize="current-password"
-            fullWidth
-            required
-          ></TextField>
-        </form>
-      </div>
-    </Container>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              label="Password"
+              id="password"
+              name="password"
+              autoCapitalize="current-password"
+              fullWidth
+              required
+            ></TextField>
+
+            <Button
+              className="submit-button"
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Sign Up
+            </Button>
+          </form>
+          <Link component={RouterLink} to="/login" variant="body2">
+            Already have an account? Sign in
+          </Link>
+        </div>
+      </Container>
+    </div>
   );
 };
 
